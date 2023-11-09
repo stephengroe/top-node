2997e2810ca2658c89cf07053991acfcc385c303
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-11-09 14:31:51 -0700

    Refactor to DRYer code

--- index.js
+++ index.js
@@ -5,30 +5,34 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer( (req, res) => {
-  let filePath;
+  let path;
+  let status = 200;
 
   switch(req.url) {
     case '/':
     case '/index':
-      filePath = __dirname + '/index.html';
+      filePath = '/index.html';
       break;
     case '/about':
-      filePath = __dirname + '/about.html';
+      filePath = '/about.html';
       break;
     case '/contact':
-      filePath = __dirname + '/contact.html';
+      filePath = '/contact.html';
       break;
     case '/style.css':
-      filePath = __dirname + '/style.css';
+      filePath = '/style.css';
       break;
     default:
-      filePath = __dirname + '/404.html';
+      filePath = '/404.html';
+      status = 404;
   }
 
+  filePath = __dirname + filePath;
+
   fs.readFile(filePath, (err, data) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(data, "utf8");
+    res.writeHead(status, {'Content-Type': 'text/html'});
+    res.write(data)
+    res.end();
   });
 });
 
